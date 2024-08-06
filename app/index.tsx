4fd731b81e986b1b8d88e4b6780c57b98d1d5c51
Aq7853gehc <aquib7583@gmail.com>
2024-08-06 16:16:11 +0530

    region added therer

--- app/index.tsx
+++ app/index.tsx
@@ -17,7 +17,6 @@ import * as Location from "expo-location";
 
 const index = () => {
   const [refreshing, setRefreshing] = React.useState(false);
-
   const onRefresh = React.useCallback(async () => {
     setRefreshing(true);
     await Location.requestForegroundPermissionsAsync();
@@ -26,8 +25,8 @@ const index = () => {
     }, 200);
   }, []);
   const permentLocation: Location.LocationObjectCoords = {
-    latitude: 28.6365945,
-    longitude: 77.2729001,
+    latitude: 28.6365382,
+    longitude: 77.2728965,
     accuracy: null,
     altitude: null,
     altitudeAccuracy: null,
@@ -41,50 +40,59 @@ const index = () => {
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
+      await Location.requestBackgroundPermissionsAsync();
+      let statusBack = await Location.isBackgroundLocationAvailableAsync()
+      if (status !== "granted" && !statusBack) {
         setUstatus(false);
         return;
       } else {
         setUstatus(true);
+        const region: Location.LocationRegion[] = [
+          {
+            identifier: "Home aquib",
+            latitude: 28.6365382,
+            longitude: 77.2728965,
+            radius: 1,
+          },
+        ];
+
+        await Location.startGeofencingAsync("entering", region);
       }
       console.log(status);
-      let location = await Location.getCurrentPositionAsync();
+      let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
     })();
   }, []);
 
   return (
     <SafeAreaView className="flex-1 h-screen w-screen">
-     
-        {ustatus &&
-        location?.coords.latitude === permentLocation?.latitude &&
-        location.coords.longitude === permentLocation.longitude ? (
-          <View className="flex-1 items-center gap-20 py-20">
-            <View className="items-center ">
-              <Text className="text-3xl font-black">Welcome to College</Text>
-              <Text>Mark your precious attendance</Text>
-            </View>
-            <View className="w-[300px] h-[300px] items-center">
-              <Image source={image.homeImage} className="w-full h-full" />
-              <View className="border-[0.3px] w-full relative bottom-5"></View>
-              <Text>{location?.coords.latitude}</Text>
-              <Text>{location?.coords.longitude}</Text>
-            </View>
-            <View className="flex-row w-full justify-evenly">
-              <Button title="Login" onPress={() => router.push("/sign-in")} />
-              <Button title="Login" onPress={() => router.push("/sign-in")} />
-            </View>
+      {ustatus ? (
+        <View className="flex-1 items-center gap-20 py-20">
+          <View className="items-center ">
+            <Text className="text-3xl font-black">Welcome to College</Text>
+            <Text>Mark your precious attendance</Text>
           </View>
-        ) : (
-          <View className="flex-1 h-full w-full items-center justify-center gap-y-10">
-            <Text className="text-3xl font-bold text-red-600">
-              Permit the location & Reach the location
-            </Text>
-            <Button title="Location" onPress={() => Linking.openSettings()} />
+          <View className="w-[300px] h-[300px] items-center">
+            <Image source={image.homeImage} className="w-full h-full" />
+            <View className="border-[0.3px] w-full relative bottom-5"></View>
             <Text>{location?.coords.latitude}</Text>
             <Text>{location?.coords.longitude}</Text>
           </View>
-        )}
+          <View className="flex-row w-full justify-evenly">
+            <Button title="Login" onPress={() => router.push("/sign-in")} />
+            <Button title="Login" onPress={() => router.push("/sign-in")} />
+          </View>
+        </View>
+      ) : (
+        <View className="flex-1 h-full w-full items-center justify-center gap-y-10">
+          <Text className="text-3xl font-bold text-red-600">
+            Permit the location & Reach the location
+          </Text>
+          <Button title="Location" onPress={() => Linking.openSettings()} />
+          <Text>{location?.coords.latitude}</Text>
+          <Text>{location?.coords.longitude}</Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
@@ -101,4 +109,3 @@ export default index;
  * logitute : 77.2729001
  * accuracy : 100
  */
-
