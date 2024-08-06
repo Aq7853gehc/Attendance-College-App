595b143f8f4d39ce74f3291d1afa0d6eb8e083f2
Aq7853gehc <aquib7583@gmail.com>
2024-08-06 15:14:29 +0530

    yoiur lo

--- app/index.tsx
+++ app/index.tsx
@@ -13,7 +13,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { image } from "@/constants";
 import { router } from "expo-router";
 import * as Location from "expo-location";
-import PermissionLocation from "@/components/PermissionLocation";
+// import PermissionLocation from "@/components/PermissionLocation";
 
 const index = () => {
   const [refreshing, setRefreshing] = React.useState(false);
@@ -28,7 +28,7 @@ const index = () => {
   const permentLocation: Location.LocationObjectCoords = {
     latitude: 28.6365945,
     longitude: 77.2729001,
-    accuracy: 50,
+    accuracy: null,
     altitude: null,
     altitudeAccuracy: null,
     heading: null,
@@ -55,32 +55,36 @@ const index = () => {
 
   return (
     <SafeAreaView className="flex-1 h-screen w-screen">
-      {ustatus  && ((location?.coords.latitude === permentLocation?.latitude)&&(location.coords.longitude ===permentLocation.longitude))? (
-        <View className="flex-1 items-center gap-20 py-20">
-          <View className="items-center ">
-            <Text className="text-3xl font-black">Welcome to College</Text>
-            <Text>Mark your precious attendance</Text>
+     
+        {ustatus &&
+        location?.coords.latitude === permentLocation?.latitude &&
+        location.coords.longitude === permentLocation.longitude ? (
+          <View className="flex-1 items-center gap-20 py-20">
+            <View className="items-center ">
+              <Text className="text-3xl font-black">Welcome to College</Text>
+              <Text>Mark your precious attendance</Text>
+            </View>
+            <View className="w-[300px] h-[300px] items-center">
+              <Image source={image.homeImage} className="w-full h-full" />
+              <View className="border-[0.3px] w-full relative bottom-5"></View>
+              <Text>{location?.coords.latitude}</Text>
+              <Text>{location?.coords.longitude}</Text>
+            </View>
+            <View className="flex-row w-full justify-evenly">
+              <Button title="Login" onPress={() => router.push("/sign-in")} />
+              <Button title="Login" onPress={() => router.push("/sign-in")} />
+            </View>
           </View>
-          <View className="w-[300px] h-[300px] items-center">
-            <Image source={image.homeImage} className="w-full h-full" />
-            <View className="border-[0.3px] w-full relative bottom-5"></View>
+        ) : (
+          <View className="flex-1 h-full w-full items-center justify-center gap-y-10">
+            <Text className="text-3xl font-bold text-red-600">
+              Permit the location & Reach the location
+            </Text>
+            <Button title="Location" onPress={() => Linking.openSettings()} />
             <Text>{location?.coords.latitude}</Text>
             <Text>{location?.coords.longitude}</Text>
           </View>
-          <View className="flex-row w-full justify-evenly">
-            <Button title="Login" onPress={() => router.push("/sign-in")} />
-            <Button title="Login" onPress={() => router.push("/sign-in")} />
-          </View>
-        </View>
-      ) :  (
-        
-        <View className="flex-1 h-full w-full items-center justify-center gap-y-10">
-          <Text className="text-3xl font-bold text-red-600">
-            Permit the location & Reach the location
-          </Text>
-          <Button title="Location" onPress={() => Linking.openSettings()} />
-        </View>
-      )}
+        )}
     </SafeAreaView>
   );
 };
@@ -97,3 +101,4 @@ export default index;
  * logitute : 77.2729001
  * accuracy : 100
  */
+
